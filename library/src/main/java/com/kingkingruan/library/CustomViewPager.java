860860dad88144b6665318f5964ac2b43aa1656e
kingkingruan <rjjdick@gmail.com>
2016-08-08 20:05:07 +0800

    新建工程

---
+++ library/src/main/java/com/kingkingruan/library/CustomViewPager.java
@@ -0,0 +1,21 @@
+package com.kingkingruan.library;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by ruanjinjing on 16/8/8.
+ */
+public class CustomViewPager extends ViewGroup {
+
+
+    public CustomViewPager(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
+
+    }
+}
